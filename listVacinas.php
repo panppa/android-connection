a15d7e70328ff277ed8beb91cf2f05362d99ac8f
pan <gabrielfcampos2017@gmail.com>
2022-01-07 08:23:29 -0300

    listando vacinacoes

---
+++ listVacinas.php
@@ -0,0 +1,22 @@
+<?php
+include "conexao.php";
+
+$cpf = $_POST['cpf'];
+
+
+$sql_read = "SELECT * FROM vacinacao WHERE cpfPaciente = 1";
+$dados = $PDO ->query($sql_read);
+$resultado;
+while($paciente  = $dados->fetch(PDO::FETCH_OBJ)){
+    $resultado = array("cpf"=>$paciente-> cpf,
+                         "nome"=>$paciente-> nome,
+                         "sobrenome"=>$paciente-> sobrenome,
+                         "email"=>$paciente-> email,
+                         "telefone"=>$paciente-> telefone,
+                         "genero"=>$paciente-> genero,
+                         "nascimento"=>$paciente-> nascimento,
+                         "senha"=>$paciente-> senha,
+                         "alergia"=>$paciente-> alergia);
+}
+echo json_encode($resultado);
+?>
